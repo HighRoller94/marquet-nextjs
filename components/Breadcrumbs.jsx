ba3dd75253fffabace6424d5018c072fc5223a40
Ash Bridges <ashbridgescodes@gmail.com>
2023-03-22 15:39:54 +0000

    added working breadcrumbs!

--- components/Breadcrumbs.jsx
+++ components/Breadcrumbs.jsx
@@ -1,30 +1,37 @@
-import Breadcrumbs from '@mui/material/Breadcrumbs';
-import Link from '@mui/material/Link';
-import Typography from '@mui/material/Typography';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
-import React from 'react';
+import { BsChevronRight } from 'react-icons/bs'
+import BreadCrumbsStyles from '../styles/components/Breadcrumbs.module.scss'
 
-export default function NextBreadcrumbs() {
-  // Gives us ability to load the current route details
+const Breadcrumbs = () => {
   const router = useRouter();
+  const pathArray = router.asPath.split('/').filter((x) => x !== '');
+
+  const breadcrumbs = pathArray.map((path, index) => ({
+    path: `/${pathArray.slice(0, index + 1).join('/')}`,
+    name: path.charAt(0).toUpperCase() + path.slice(1),
+  }));
+
 
   return (
-    <Breadcrumbs aria-label="breadcrumb" />
+    <nav aria-label="Breadcrumb" className={BreadCrumbsStyles.container}>
+      <ol className={BreadCrumbsStyles.breadcrumbs}>
+        <li className="breadcrumb-item">
+          <Link href="/">
+            Home
+          </Link>
+        </li>
+        {breadcrumbs.map((breadcrumb, index) => (
+          <li key={index} className="breadcrumb-item">
+            <BsChevronRight className={BreadCrumbsStyles.icon} />
+            <Link href={breadcrumb.path}>
+              {breadcrumb.name}
+            </Link>
+          </li>
+        ))}
+      </ol>
+    </nav>
   );
-}
+};
 
-
-// Each individual "crumb" in the breadcrumbs list
-function Crumb({ text, href, last=false }) {
-  // The last crumb is rendered as normal text since we are already on the page
-  if (last) {
-    return <Typography color="text.primary">{text}</Typography>
-  }
-
-  // All other crumbs will be rendered as links that can be visited 
-  return (
-    <Link underline="hover" color="inherit" href={href}>
-      {text}
-    </Link>
-  );
-}
+export default Breadcrumbs;
