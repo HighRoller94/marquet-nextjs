bac0e0ba3af15385fed18550cb4364958aeab13a
Ash Bridges <ashbridgescodes@gmail.com>
2023-03-22 15:39:29 +0000

    introduced slick carousel

--- components/Carousel.jsx
+++ components/Carousel.jsx
@@ -1,55 +1,117 @@
 import React from 'react'
 import Image from 'next/image'
 
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import Carousel from "react-responsive-carousel/lib/js/components/Carousel/index";
+import Slider from 'react-slick';
 
 import CarouselStyles from '../styles/components/Carousel.module.scss'
 
 const CarouselHolder = () => {
-  return (
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true,
+              },
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                initialSlide: 1,
+              },
+            },
+          ],
+    };
 
+    return (
         <div className={CarouselStyles.carousel}>
-        <Carousel
-            className={CarouselStyles.carousel}
-            animationHandler="fade"
-            autoPlay={false}
-            transitionTime="2000"
-            interval="false"
-            showArrows={false}
-            infiniteLoop
-            showStatus={false}
-            showIndicators={false}
-            swipeable={true}
-            showThumbs={false}
-            >
-
-            <div className={CarouselStyles.inner}>
-                <div className={CarouselStyles.imageContainer}>
-                    <Image
-                        src="/images/vans__bg.svg"
-                        fill
-                        loading="lazy"
-                        alt="Vans Hero"
-                    />
-                </div>
-                <div className={CarouselStyles.container}>
-                    <div className={CarouselStyles.logoContainer}>
+            <Slider {...settings}>
+                <div className={CarouselStyles.inner}>
+                    <div className={CarouselStyles.imageContainer}>
                         <Image
-                            src="/images/vans__logo.svg"
+                            src="/images/vans__bg.svg"
                             fill
                             loading="lazy"
                             alt="Vans Hero"
                         />
                     </div>
-                    <div className={CarouselStyles.header}>
-                        <h1>Up to 30% off</h1>
-                        <button className="main__btn">Shop now</button>
+                    <div className={CarouselStyles.container}>
+                        <div className={CarouselStyles.logoContainer}>
+                            <Image
+                                src="/images/vans__logo.svg"
+                                fill
+                                loading="lazy"
+                                alt="Vans Hero"
+                            />
+                        </div>
+                        <div className={CarouselStyles.header}>
+                            <h1>Up to 30% off</h1>
+                            <button className="main__btn">Shop now</button>
+                        </div>
                     </div>
-                </div>
 
-            </div>
-        </Carousel>
+                </div>
+                <div className={CarouselStyles.inner}>
+                    <div className={CarouselStyles.imageContainer}>
+                        <Image
+                            src="/images/vans__bg.svg"
+                            fill
+                            loading="lazy"
+                            alt="Vans Hero"
+                        />
+                    </div>
+                    <div className={CarouselStyles.container}>
+                        <div className={CarouselStyles.logoContainer}>
+                            <Image
+                                src="/images/vans__logo.svg"
+                                fill
+                                loading="lazy"
+                                alt="Vans Hero"
+                            />
+                        </div>
+                        <div className={CarouselStyles.header}>
+                            <h1>Up to 30% off</h1>
+                            <button className="main__btn">Shop now</button>
+                        </div>
+                    </div>
+
+                </div>
+                <div className={CarouselStyles.inner}>
+                    <div className={CarouselStyles.imageContainer}>
+                        <Image
+                            src="/images/vans__bg.svg"
+                            fill
+                            loading="lazy"
+                            alt="Vans Hero"
+                        />
+                    </div>
+                    <div className={CarouselStyles.container}>
+                        <div className={CarouselStyles.logoContainer}>
+                            <Image
+                                src="/images/vans__logo.svg"
+                                fill
+                                loading="lazy"
+                                alt="Vans Hero"
+                            />
+                        </div>
+                        <div className={CarouselStyles.header}>
+                            <h1>Up to 30% off</h1>
+                            <button className="main__btn">Shop now</button>
+                        </div>
+                    </div>
+
+                </div>
+            </Slider>
         </div>
     )
 }
