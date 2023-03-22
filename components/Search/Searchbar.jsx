aece6124f1fcdc058189884216e90cafdcc26219
Ash Bridges <ashbridgescodes@gmail.com>
2023-03-22 23:32:01 +0000

    updated autocomplete for searchbar to products

--- components/Search/Searchbar.jsx
+++ components/Search/Searchbar.jsx
@@ -1,48 +1,61 @@
 import React, { useState } from 'react'
 
+import { useRouter } from 'next/router'
+
 import { HiOutlineSearch } from 'react-icons/hi'
 
 import SearchStyles from '../../styles/components/Search.module.scss';
 
 const Searchbar = ({ setResults }) => {
-    const [input, setInput] = useState("")
+  const router = useRouter()
+  const [input, setInput] = useState("")
+  const [query, setQuery] = useState('');
+  
+  const fetchData = (value) => {
+    fetch("http://localhost:3000/api/products")
+      .then((response) => response.json())
+      .then((json) => {
+        const results = json.filter((user) => {
+          return (
+            value &&
+            user &&
+            user.name &&
+            user.name.toLowerCase().includes(value)
+          );
+        });
+        setResults(results);
+      });
+  };
 
-    const fetchData = (value) => {
-        fetch("https://jsonplaceholder.typicode.com/users")
-          .then((response) => response.json())
-          .then((json) => {
-            const results = json.filter((user) => {
-              return (
-                value &&
-                user &&
-                user.name &&
-                user.name.toLowerCase().includes(value)
-              );
-            });
-            setResults(results);
-          });
-      };
+  const handleChange = (value) => {
+    setInput(value);
+    fetchData(value);
+  };
 
-      const handleChange = (value) => {
-        setInput(value);
-        fetchData(value);
-      };
-    
+  const searchMarquet = (e) => {
+    let str = input;
+    e.preventDefault()
+    let cleanStr = str.replace(/\s/g, "+")
+    console.log(cleanStr)
+    router.push(`/search?=${cleanStr}`)
+    setInput("")
+    setResults("")
+  }
 
-    return (
-      <>
-        <div className={SearchStyles.searchWrapper}>
-            <HiOutlineSearch className={SearchStyles.searchIcon}/>
-            <input 
-                type="text"
-                value={input} 
-                onChange={(e) => handleChange(e.target.value)}
-            />
-            <div className={SearchStyles.overlay}></div>
-        </div>
-        
-        </>
-    )
+  return (
+    <>
+      <form onSubmit={searchMarquet} className={SearchStyles.searchWrapper}>
+        <HiOutlineSearch className={SearchStyles.searchIcon} />
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => handleChange(e.target.value)}
+        />
+        <div className={SearchStyles.overlay}></div>
+      </form>
+
+    </>
+  )
 }
 
 export default Searchbar
