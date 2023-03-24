fbd74ea64b5e7eb8dfbc4cf4cc76ecc093b127f9
Ash Bridges <ashbridgescodes@gmail.com>
2023-03-24 13:19:54 +0000

    updated breadcrumbs to accept params

--- components/Breadcrumbs.jsx
+++ components/Breadcrumbs.jsx
@@ -1,16 +1,19 @@
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 import { BsChevronRight } from 'react-icons/bs'
 import BreadCrumbsStyles from '../styles/components/Breadcrumbs.module.scss'
+import { useRouter } from 'next/router';
 
-const Breadcrumbs = () => {
+const Breadcrumbs = ({ name }) => {
   const router = useRouter();
   const pathArray = router.asPath.split('/').filter((x) => x !== '');
+  const currentPath = router.asPath.toLowerCase();
+  const query = Object.values(router.query)[0];
 
   const breadcrumbs = pathArray.map((path, index) => ({
     path: `/${pathArray.slice(0, index + 1).join('/')}`,
     name: path.charAt(0).toUpperCase() + path.slice(1),
   }));
+
 
 
   return (
@@ -21,14 +24,32 @@ const Breadcrumbs = () => {
             Home
           </Link>
         </li>
-        {breadcrumbs.map((breadcrumb, index) => (
-          <li key={index} className="breadcrumb-item">
-            <BsChevronRight className={BreadCrumbsStyles.icon} />
-            <Link href={breadcrumb.path}>
-              {breadcrumb.name}
-            </Link>
-          </li>
-        ))}
+        {name ? (
+            <>
+              <li className={BreadCrumbsStyles.wrapper}>
+                <BsChevronRight className={BreadCrumbsStyles.icon} />
+                <span>{name}</span>
+              </li>
+            </>
+        ) : (
+          <>
+          {breadcrumbs.map((breadcrumb, index) => (
+            <li key={index} className={BreadCrumbsStyles.wrapper}>
+              <BsChevronRight className={BreadCrumbsStyles.icon} />
+              {currentPath.includes('search') ? (
+                <span>
+                  Search results for "{query}"
+                </span>
+              ) : (
+                  <Link href={breadcrumb.path}>
+                    {breadcrumb.name}
+                  </Link>
+              )}
+            </li>
+          ))}
+          </>
+        )}
+        
       </ol>
     </nav>
   );
