b447ba76678baebb4fff78fc6591fb1e9f484998
Ash Bridges <ashbridgescodes@gmail.com>
2023-03-30 22:54:06 +0100

    added timer component, removed bloat

---
+++ components/CountdownTimer.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+
+const CountdownTimer = ({ seconds }) => {
+  const [timeLeft, setTimeLeft] = useState(seconds);
+
+  useEffect(() => {
+    if (timeLeft === 0) return;
+
+    const intervalId = setInterval(() => {
+      setTimeLeft(prevTimeLeft => prevTimeLeft - 1);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, [timeLeft]);
+
+  const formatTime = time => (time < 10 ? `0${time}` : time);
+
+  const hours = Math.floor(timeLeft / 3600);
+  const minutes = Math.floor((timeLeft % 3600) / 60);
+  const second = timeLeft % 60;
+
+  return (
+    <span>
+      {formatTime(hours)}:{formatTime(minutes)}:{formatTime(second)}
+    </span>
+  );
+};
+
+export default CountdownTimer;
