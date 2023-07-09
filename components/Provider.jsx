a24a145a268d12522b652de333199c5a1b98924e
Ash Bridges <ashbridgescodes@gmail.com>
2023-07-10 00:36:17 +0100

    added redux persist

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -3,11 +3,19 @@
 import { SessionProvider } from "next-auth/react";
 import store from "@/redux/store";
 import { Provider as Redux } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
+let persister = persistStore(store)
 const Provider = ({ children }) => {
   return (
     <Redux store={store}>
-      <SessionProvider>{children}</SessionProvider>
+      <SessionProvider>
+        <PersistGate persistor={persister}>
+                  {children}
+        </PersistGate>
+
+        </SessionProvider>
     </Redux>
   );
 };
--- redux/store.js
+++ redux/store.js
@@ -1,10 +1,45 @@
-import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './cartSlice'
-import authReducer from './authSlice'
+import { configureStore } from "@reduxjs/toolkit";
+
+import { persistReducer } from "redux-persist";
+import { combineReducers } from "@reduxjs/toolkit";
+
+import cartReducer from "./cartSlice";
+import authReducer from "./authSlice";
+
+import createwebStorage from "redux-persist/lib/storage/createwebStorage";
+
+const createNoopStorage = () => {
+  return {
+    getIten(_key) {
+      return Promise.resolve(null);
+    },
+    setItem(_key, value) {
+      return Promise.resolve(value);
+    },
+    removelten(_key) {
+      return Promise.resolve();
+    },
+  };
+};
+
+const storage =
+  typeof window !== "undefined"
+    ? createwebStorage("local")
+    : createNoopStorage();
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const reducer = combineReducers({
+  auth: authReducer,
+  cart: cartReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 export default configureStore({
-    reducer: {
-        auth: authReducer,
-        cart: cartReducer,
-    }
-})
+  reducer: persistedReducer,
+});
