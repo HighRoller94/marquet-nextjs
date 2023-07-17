c4b05188dd5f254c9a3e9068f1790d601210ba7e
Ash Bridges <ashbridgescodes@gmail.com>
2023-07-17 10:55:49 +0100

    carousel overlay change

--- components/Carousel.jsx
+++ components/Carousel.jsx
@@ -28,7 +28,7 @@ const CarouselHolder = () => {
     >
       {CarouselContent?.map((slide, i) => (
         <div className="w-full h-full relative">
-          <div className="relative w-full h-[500px] bg-neutral-200">
+          <div className="relative w-full h-[500px] bg-neutral-200 after:absolute after:w-full after:h-full after:top-0 after:left-0 after:bg-neutral-900 after:opacity-20">
             <Image
               fill
               src={slide.imageSrc}
@@ -37,7 +37,7 @@ const CarouselHolder = () => {
             />
           </div>
           <div className="p-2 rounded-lg absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-10/12 flex flex-col items-center justify-center text-center -mt-4">
-            <h1 className="mt-4 font-black text-white text-5xl capitalize leading-tight lg:text-6xl">{slide.header}</h1>
+            <h1 className="mt-4 font-extrabold text-white text-5xl capitalize leading-tight lg:text-6xl">{slide.header}</h1>
             <p className=" text-white font-medium mt-4 lg:mt-2 text-sm w-5/6 lg:text-lg">{slide.body}</p>
             <button className="w-fit bg-neutral-950 text-white border-none mt-10 outline-none font-bold transition px-6 lg:mt-6 rounded-sm py-2 lg:text-lg">Let's Shop</button>
           </div>
