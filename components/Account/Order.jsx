6d57993c06970c76f8719ee7e8f117a39b01aa68
Ash Bridges <ashbridgescodes@gmail.com>
2023-07-18 11:50:01 +0100

    responsive order component

--- components/Account/Order.jsx
+++ components/Account/Order.jsx
@@ -11,14 +11,14 @@ function Order({ order, index }) {
 
   function calculateProgress(startDate, endDate) {
     const totalDuration = endDate.diff(startDate);
-  
+
     const currentDate = dayjs();
     const currentDuration = currentDate.diff(startDate);
     let progressPercentage = Math.round(
       (currentDuration / totalDuration) * 100
     );
-  
-    console.log(progressPercentage)
+
+    console.log(progressPercentage);
     if (progressPercentage < 33) {
       return `w-[30%]`;
     } else if (progressPercentage >= 33 && progressPercentage < 66) {
@@ -27,33 +27,42 @@ function Order({ order, index }) {
       return `w-[${progressPercentage}%]`;
     }
   }
-  
 
   const startDate = dayjs(orderDate);
   const endDate = dayjs(deliveryDate);
-  
+
   const progress = calculateProgress(startDate, endDate);
   return (
     <div key={index} className="w-full bg-white p-8 mb-6">
-      <div className="w-12/12 h-4 bg-neutral-200 rounded mb-2">
+      <div className="w-12/12 h-3.5  bg-neutral-200 rounded-full mb-2">
         <div
-          className={`h-full rounded-md transtion-all duration-500 ease-in-out bg-neutral-800 ${progress}`}
+          className={`h-full rounded-full transtion-all duration-500 ease-in-out bg-green-400 ${progress}`}
         ></div>
       </div>
       <div className="flex flex-col md:flex-row justify-between b-2 border-b py-4">
-        <div className="w-full flex flex-col md:flex-row gap-x-2  text-neutral-700 uppercase tracking-wide font-medium">
-          <h1 className="text-neutral-400 font-bold">Order Status:</h1>
-          {progress === 100 ? <p>Delivered</p> : (progress < 33 ? <p>We've received your order</p> : <p>Out for delivery</p>)}
-        </div>
-        <div className="flex w-fit flex-col md:flex-row gap-x-2  text-neutral-700 uppercase tracking-wide font-medium">
-          <h1 className="flex flex-nowrap whitespace-nowrap text-neutral-400 font-bold">
-            ESTIMATED DELIVERY:
+        <div className="w-full flex flex-col md:flex-row gap-x-2 items-center text-neutral-700 tracking-wide">
+          <h1 className="text-neutral-400 font-bold text-sm py-2 uppercase ">
+            Order Status:
           </h1>
-          <p>{order.deliveryDate}</p>
+          {progress === 100 ? (
+            <p className="font-bold -mt-0.5">Delivered</p>
+          ) : progress < 33 ? (
+            <p className="font-bold -mt-0.5">We've received your order</p>
+          ) : (
+            <p className="font-bold -mt-0.5">Out for delivery</p>
+          )}
+        </div>
+        <div className="flex w-fit flex-col md:flex-row gap-x-2 items-center  text-neutral-700 uppercase tracking-wide">
+          <h1 className="flex flex-nowrap whitespace-nowrap py-2 text-neutral-400 text-sm font-bold">
+            Estimated Delivery:
+          </h1>
+          <p className="font-bold -mt-0.5">{order.deliveryDate}</p>
         </div>
       </div>
       <div className="flex b-2 border-b pb-4 flex-col  text-neutral-500 uppercase tracking-wide font-bold">
-        <h1 className="pt-4">{order.products.length} Item{order.products.length > 1 ? "s" : ""}</h1>{" "}
+        <h1 className="pt-4">
+          {order.products.length} Item{order.products.length > 1 ? "s" : ""}
+        </h1>{" "}
         <div className="flex gap-x-4 my-4">
           {order.products.map((product, i) => (
             <div key={i} className="relative w-40 h-40">
@@ -70,16 +79,16 @@ function Order({ order, index }) {
       </div>
       <div className="flex justify-between w-full pt-4 ">
         <div className="flex flex-col gap-y-2">
-          <div className="flex gap-2  text-neutral-500 uppercase tracking-wide font-bold">
-            <p>Order Reference:</p>
-            <p>{order.orderReference}</p>
+          <div className="flex gap-2 flex-col md:flex-row  uppercase tracking-wide font-bold">
+            <p className="py-1 text-sm  text-neutral-500">Order Reference:</p>
+            <p className="text-neutral-700">{order.orderReference}</p>
           </div>
-          <div className="flex gap-2  text-neutral-500 uppercase tracking-wide font-bold">
-            <p>Order date:</p>
-            <p>{order.orderDate}</p>
+          <div className="flex gap-2 flex-col md:flex-row   uppercase tracking-wide font-bold">
+            <p className="py-1 text-sm text-neutral-500">Order date:</p>
+            <p className="text-neutral-700">{order.orderDate}</p>
           </div>
         </div>
-        <button className=" px-6 py-4 bg-neutral-400 uppercase tracking-widest font-semibold text-white hover:bg-neutral-600 transition rounded">
+        <button className="hidden lg:flex px-6 py-3 items-center h-fit justify-center bg-neutral-400 uppercase tracking-widest font-semibold text-white hover:bg-neutral-600 transition rounded">
           View Order
         </button>
       </div>
