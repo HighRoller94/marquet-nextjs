6b53d4fee877224d719c338fd2351482a461b263
Ash Bridges <ashbridgescodes@gmail.com>
2023-09-24 22:06:01 +0100

    fixed key prop error

--- components/Basket/BasketProductList.jsx
+++ components/Basket/BasketProductList.jsx
@@ -5,20 +5,21 @@ const BasketProductList = ({ products, paramQuery }) => {
   return (
     <div className="grid grid-cols-2 lg:grid-cols-3 sm:gap-8 sm:my-10 gap-3">
       {products?.map((product, i) => (
-        <BasketProduct
-          key={i}
-          paramQuery={paramQuery}
-          selectedSize={product.sizeSelected}
-          quantity={product.quantity}
-          id={product._id}
-          product={product}
-          index={i}
-          name={product.name}
-          mainImage={product.mainImage}
-          type={product.type}
-          price={product.price}
-          gallery={product.gallery}
-        />
+        <div key={i}>
+          <BasketProduct
+            paramQuery={paramQuery}
+            selectedSize={product.sizeSelected}
+            quantity={product.quantity}
+            id={product._id}
+            product={product}
+            index={i}
+            name={product.name}
+            mainImage={product.mainImage}
+            type={product.type}
+            price={product.price}
+            gallery={product.gallery}
+          />
+        </div>
       ))}
     </div>
   );
--- components/Basket/CheckoutProductList.jsx
+++ components/Basket/CheckoutProductList.jsx
@@ -4,20 +4,21 @@ const CheckoutProductList = ({ products, paramQuery }) => {
   return (
     <div className="flex flex-col sm:gap-8  gap-1">
       {products?.map((product, i) => (
-        <CheckoutProduct
-          paramQuery={paramQuery}
-          selectedSize={product.sizeSelected}
-          key={product.name}
-          quantity={product.quantity}
-          id={product._id}
-          product={product}
-          index={i}
-          name={product.name}
-          mainImage={product.mainImage}
-          type={product.type}
-          price={product.price}
-          gallery={product.gallery}
-        />
+        <div key={i}>
+          <CheckoutProduct
+            paramQuery={paramQuery}
+            selectedSize={product.sizeSelected}
+            quantity={product.quantity}
+            id={product._id}
+            product={product}
+            index={i}
+            name={product.name}
+            mainImage={product.mainImage}
+            type={product.type}
+            price={product.price}
+            gallery={product.gallery}
+          />
+        </div>
       ))}
     </div>
   );
